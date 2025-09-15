16c4e43393deb8b99689e5e19846bb7cc7e07c64
KaykyOne <kaykyzioti@gmail.com>
2025-09-15 16:39:10 -0300

    att correção .env

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,12 @@
+import { writeFileSync } from 'fs';
+import { config } from 'dotenv';
+
+config();
+
+const content = `
 export const environment = {
   production: false,
-  apiUrl: 'https://ztkhqkxohahwmnxnmpah.supabase.co',
-  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0a2hxa3hvaGFod21ueG5tcGFoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1Njc1NzUxOSwiZXhwIjoyMDcyMzMzNTE5fQ.J1H9oLxrFiMwDVH_1_Mt1Yb-zfD_ccow_IXAcd39bBQ'
+  apiUrl: "${process.env.API_URL}",
+  apiKey: "${process.env.API_KEY}"
 };
+`;
